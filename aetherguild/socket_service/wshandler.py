5b655de39fcfe5209786f82a65bfd61349a3cb9b
Tuomas Virtanen <katajakasa@gmail.com>
2016-09-06 02:12:35 +0300

    Never mind.

--- aetherguild/socket_service/wshandler.py
+++ aetherguild/socket_service/wshandler.py
@@ -21,7 +21,11 @@ class WsHandler(WebSocketHandler):
         self.user_level = 0
 
     def check_origin(self, origin):
-        return True
+        if config.DEBUG:
+            return True
+        else:
+            parsed_origin = urlparse(origin)
+            return parsed_origin.hostname in config.ALLOW_HOSTS
 
     def open(self):
         """ Handler for opened websocket connections """
