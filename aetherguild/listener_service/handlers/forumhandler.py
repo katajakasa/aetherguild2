9e2585f40236e9552eac1c74f65f9a945e0a4c47
Tuomas Virtanen <katajakasa@gmail.com>
2016-09-10 01:49:52 +0300

    Update thread.views counter as necessary

--- aetherguild/listener_service/handlers/forumhandler.py
+++ aetherguild/listener_service/handlers/forumhandler.py
@@ -234,6 +234,10 @@ class ForumHandler(BaseHandler):
                 data['edits'].append(edit.serialize())
             post_list.append(data)
 
+        # Increment thread views count
+        thread.views += 1
+        self.db.add(thread)
+
         self.send_message({
             'board': board.serialize(),
             'thread': thread.serialize(),
